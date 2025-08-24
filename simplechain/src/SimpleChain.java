e047c80f51f6f50a4838ebc7ad7e32636f945b08
mepha89 <umamarahman12@gmail.com>
2025-08-24 17:18:12 -0230

    added-isChainValid-func

--- simplechain/src/SimpleChain.java
+++ simplechain/src/SimpleChain.java
@@ -5,6 +5,30 @@ public class SimpleChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        // loop through blockchain to check hashes
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            // compare registered hash and calculated hash
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal!");
+                return false;
+            }
+
+            // compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal!");
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello there, World!\n"); // TODO: remove after testing
 
