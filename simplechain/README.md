e49c3c9a1c1ffda42f00b3aa0f57af8ba0b800c9
mepha89 <umamarahman12@gmail.com>
2025-08-24 16:55:53 -0230

    test-simplechain-init

--- simplechain/README.md
+++ simplechain/README.md
@@ -8,6 +8,20 @@ The workspace contains two folders by default, where:
 
 Meanwhile, the compiled output files will be generated in the `bin` folder by default.
 
+## Steps to run the project
+
+1. Compile using:
+
+    ```shell
+    javac -d bin src/*.java
+    ```
+
+1. Run using:
+
+    ```shell
+    java -cp bin SimpleChain
+    ```
+
 ## Dependency Management
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
--- simplechain/src/SimpleChain.java
+++ simplechain/src/SimpleChain.java
@@ -1,6 +1,18 @@
 public class SimpleChain {
-    // TODO: Implement SimpleChain class
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello there, World!"); // TODO: remove after testing
+
+        // first block
+        Block genesisBlock = new Block("Hi, I am the first block", "0");
+        System.out.println("Hash for block 1: " + genesisBlock.hash);
+
+        // second block
+        Block secondBlock = new Block("Yo, I am the second block", genesisBlock.hash);
+        System.out.println("Hash for block 2: " + secondBlock.hash);
+
+        // third block
+        Block thirdBlock = new Block("Hey, I am the third block", secondBlock.hash);
+        System.out.println("Hash for block 3: " + thirdBlock.hash);
+
     }
 }
