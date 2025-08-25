591cc1e85a427a38905edb272bd93a4a3d06ddb9
mepha89 <umamarahman12@gmail.com>
2025-08-25 12:01:56 -0230

    init-wallet

--- simplechain/src/simplechain/Wallet.java
+++ simplechain/src/simplechain/Wallet.java
@@ -1,9 +1,32 @@
 package simplechain;
 
 import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
     public PrivateKey privateKey;
     public PublicKey publicKey;
 
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+
+            // init the key generator and generate a KeyPair
+            keyGen.initialize(ecSpec, random); // 256 bytes provides acceptable security
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            // set public and private keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
